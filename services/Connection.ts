2fab7fe1b8834c7cb131d480e3c4c314f4d69a2f
TatoBig <tatobig@gmail.com>
2022-10-29 17:27:21 -0600

    Added new function to fetch python API

--- services/Connection.ts
+++ services/Connection.ts
@@ -1,9 +1,40 @@
 export const _url = 'http://127.0.0.1:8000/api/v1'
 
-export const postOptions = (body) => {
-  return {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' }
+type Methods = 'GET' | 'POST'
+
+type Tables =
+  | 'student'
+  | 'professor'
+  | 'course'
+  | 'qr'
+  | 'device'
+  | 'device'
+  | 'assistance'
+  | 'faculty'
+  | 'course_student'
+  | 'assistance_category'
+
+export const fetchAPI = async <T>(
+  table: Tables,
+  method: Methods = 'GET',
+  body?: T
+) => {
+  try {
+    const response = await fetch(`${_url}/${table}/`, {
+      method,
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    const data: T = await response.json()
+
+    return {
+      ok: response.ok,
+      data
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      ok: false
+    }
   }
 }
